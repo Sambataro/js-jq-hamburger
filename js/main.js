5245058e62acc64df38b7735c4bcfe616a3057f1
fortunato <sambataro1990@gmail.com>
2020-11-23 14:41:51 +0100

    aggiunta versione con show e hide

--- js/main.js
+++ js/main.js
@@ -2,11 +2,21 @@
 // mostrare / nascondere il menu principale
 // (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
 
-
 $(".header-right").click(function() {
- $(".hamburger-menu").toggleClass("active");
+ $(".hamburger-menu").show("active");
 });
 
 $(".close").click(function() {
- $(".hamburger-menu").toggleClass("active");
+ $(".hamburger-menu").hide("active");
 });
+
+
+
+// prima versione che ho fatto dell'esercizio
+// $(".header-right").click(function() {
+//  $(".hamburger-menu").toggleClass("active");
+// });
+//
+// $(".close").click(function() {
+//  $(".hamburger-menu").toggleClass("active");
+// });
