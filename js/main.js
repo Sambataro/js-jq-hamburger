859fa6b53909085bc1336ea379964bd4f1733558
fortunato <sambataro1990@gmail.com>
2020-11-23 14:52:18 +0100

    esercizio completato
    aggiunta versione con fadein e fadeout e corretto show e hide

--- js/main.js
+++ js/main.js
@@ -2,21 +2,39 @@
 // mostrare / nascondere il menu principale
 // (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
 
+
+// prima versione esercizio con toggleClass
+
 $(".header-right").click(function() {
- $(".hamburger-menu").show("active");
+ $(".hamburger-menu").toggleClass("active");
 });
 
 $(".close").click(function() {
- $(".hamburger-menu").hide("active");
+ $(".hamburger-menu").toggleClass("active");
 });
+// prima versione esercizio--------------------//
 
 
+// seconda versione esercizio con show e hide
 
-// prima versione che ho fatto dell'esercizio
 // $(".header-right").click(function() {
-//  $(".hamburger-menu").toggleClass("active");
+//  $(".hamburger-menu").show();
 // });
 //
 // $(".close").click(function() {
-//  $(".hamburger-menu").toggleClass("active");
+//  $(".hamburger-menu").hide();
 // });
+
+// seconda versione esercizio--------------------//
+
+// terza versione esercizio con fadein e fadeout
+
+// $(".header-right").click(function() {
+//  $(".hamburger-menu").fadeIn(2000);
+// });
+//
+// $(".close").click(function() {
+//  $(".hamburger-menu").fadeOut(1000);
+// });
+
+// terza versione esercizio--------------------//
